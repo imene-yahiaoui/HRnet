ff89dbec4a47cc4a096f08b3ed1fd18efa9d4dda
imene ikhlef <imeneikhlefyahiaoui@users.noreply.github.com>
2024-04-22 23:19:22 +0200

    Add State component

--- src/components/labeledSelect/index.tsx
+++ src/components/labeledSelect/index.tsx
@@ -1,17 +1,21 @@
 import "./style.css";
-
+import { FaCaretDown } from "react-icons/fa";
 function LabeledSelect({ name, nameId, options }) {
   return (
-    <>
+    <div>
       <label htmlFor={nameId}>{name}</label>
-      <select name={nameId} id={nameId}>
-        {options.map((option) => (
-          <option key={option.key} value={option.key}>
-            {option.label}
-          </option>
-        ))}
-      </select>
-    </>
+      <div className="select-container">
+        <select name={nameId} id={nameId} className="selectBox">
+          {options.map((option) => (
+            <option key={option.key} value={option.key}>
+              {option.label}
+            </option>
+          ))}
+        </select>
+
+        <FaCaretDown className="icon" />
+      </div>
+    </div>
   );
 }
 export default LabeledSelect;
