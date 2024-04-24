f216bb1dbfdb565c356ca41d2b78e599f69d8a05
imene ikhlef <imeneikhlefyahiaoui@users.noreply.github.com>
2024-04-24 23:47:39 +0200

    add CalendarComponent

---
+++ src/components/calendarComponent/index.tsx
@@ -0,0 +1,114 @@
+import Calendar from "react-calendar";
+import LabeledInput from "../labeledInput";
+import { useState, useEffect } from "react";
+import "./style.css";
+import "react-calendar/dist/Calendar.css";
+
+type ValuePiece = Date | null;
+
+type Value = ValuePiece | [ValuePiece, ValuePiece];
+
+function CalendarComponent() {
+  const [value, onChange] = useState<Value>(new Date());
+  const [isCalendarOpen, setIsCalendarOpen] = useState<boolean>(false);
+  const [dateValue, setDateValue] = useState<string | null>(null);
+  const [ishandelInside, setIshandelInside] = useState<boolean>(false);
+  /**
+   * function close calender
+   */
+  const toggleCalendar = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    setIsCalendarOpen(!isCalendarOpen);
+    console.log("calendar is:", isCalendarOpen);
+  };
+  /**
+   * userEffect for examen is i click outside the calender or not ?if yes then close it.
+   */
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        isCalendarOpen &&
+        !document
+          .getElementById("calendar-container")
+          ?.contains(event.target as Node) &&
+        !document
+          .getElementById("date-of-birth")
+          ?.contains(event.target as Node)
+      ) {
+        setIsCalendarOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isCalendarOpen]);
+  const inputValue = document.getElementById("date-of-birth");
+
+  /**
+   * if i click in the calender
+   */
+  useEffect(() => {
+    const handleClickInside = (event: MouseEvent) => {
+      const calendarTiles = document.querySelectorAll(
+        ".react-calendar__tile.react-calendar__month-view__days__day"
+      );
+      calendarTiles.forEach((tile) => {
+        if (tile.contains(event.target as Node)) {
+          setIshandelInside(!ishandelInside);
+          console.log("ishandelInside", ishandelInside);
+        }
+      });
+    };
+
+    document.addEventListener("mousedown", handleClickInside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickInside);
+    };
+  }, [isCalendarOpen, ishandelInside]);
+
+  useEffect(() => {
+    if (isCalendarOpen && ishandelInside) {
+      console.log("je suis rentrer dans la boucle ");
+      console.log("value", value);
+      const formattedDate = `${String(value?.getDate()).padStart(
+        2,
+        "0"
+      )}/${String(value?.getMonth() + 1).padStart(
+        2,
+        "0"
+      )}/${value?.getFullYear()}`;
+      setDateValue(formattedDate);
+      inputValue.value = dateValue;
+    }
+  }, [dateValue, inputValue, isCalendarOpen, ishandelInside, value]);
+
+  return (
+    <>
+      <div>
+        <p> Date of Birth </p>
+        <button title="date" onClick={toggleCalendar} className="inputDate-btn">
+          <LabeledInput type="text" nameId="date-of-birth" />
+        </button>
+      </div>
+      {isCalendarOpen && (
+        <div id="calendar-container">
+          <Calendar
+            onChange={(date: ValuePiece) => {
+             
+              onChange(date);
+            
+            }}
+            
+            value={value as Date}
+          />
+        </div>
+      )}
+    </>
+  );
+}
+export default CalendarComponent;
--- src/containers/form/index.tsx
+++ src/containers/form/index.tsx
@@ -1,27 +1,59 @@
 import LabeledInput from "../../components/labeledInput";
 
 import Department from "../../components/department";
-import Button from "../../components/button";
 import State from "../state";
 import "./style.css";
+// import { useState } from 'react';
+ import  CalendarComponent from "../../components/calendarComponent";
+import Button from '../../components/button/index';
+
+ 
+
 function Form() {
-  const handleClick = () => {
+  const handleClick = (e) => {
+    e.preventDefault
      console.log("Button clicked!");
   };
+
+
   return (
     <div className="container">
       <form>
-        <LabeledInput type="text" name="First Name" nameId="first-name" />
-        <LabeledInput type="text" name="Last Name" nameId="last-name" />
-        <LabeledInput type="text" name="Date of Birth" nameId="date-of-birth" />
-        <LabeledInput type="text" name="Start Date" nameId="start-date" />
+        <LabeledInput 
+          type="text" 
+          name="First Name" 
+          nameId="first-name" 
+        />
+        <LabeledInput 
+          type="text" 
+          name="Last Name" 
+          nameId="last-name" 
+        />
+<CalendarComponent/>
+        <LabeledInput 
+          type="text" 
+          name="Start Date" 
+          nameId="start-date" 
+        />
 
         <fieldset className="address">
           <legend>Address</legend>
-          <LabeledInput type="text" name="Street" nameId="street" />
-          <LabeledInput type="text" name="City" nameId="city" />
+          <LabeledInput 
+            type="text" 
+            name="Street" 
+            nameId="street" 
+          />
+          <LabeledInput 
+            type="text" 
+            name="City" 
+            nameId="city" 
+          />
           <State />
-          <LabeledInput type="number" name="Zip Code" nameId="zip-code" />
+          <LabeledInput 
+            type="number" 
+            name="Zip Code" 
+            nameId="zip-code" 
+          />
         </fieldset>
         <Department />
       </form>
@@ -29,4 +61,5 @@ function Form() {
     </div>
   );
 }
+
 export default Form;
