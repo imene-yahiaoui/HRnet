aea94e10d97b84ef802e02e4dab018a31e30d729
imene ikhlef <imeneikhlefyahiaoui@users.noreply.github.com>
2024-06-27 15:39:07 +0200

    update cypress tests

--- src/pages/Employees/employees.test.tsx
+++ src/pages/Employees/employees.test.tsx
@@ -1,21 +1,26 @@
-import { screen, render} from "@testing-library/react";
+import { screen, render } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import Employees from "./index";
 import { Provider } from "react-redux";
 import { store } from "../../store.ts";
-import {  MemoryRouter } from "react-router-dom";
+import { MemoryRouter } from "react-router-dom";
 
-test("render Employees page", () => {
-  render(
-    <Provider store={store}>
-         <MemoryRouter>
-      <Employees />
-      </MemoryRouter>
-    </Provider>,
-  );
+describe("Employees component", () => {
+  test("render Employees page", () => {
+    render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <Employees />
+        </MemoryRouter>
+      </Provider>
+    );
 
-  expect(screen.getByText("Current Employees")).toBeInTheDocument();
-  expect(screen.getByText("Search:")).toBeInTheDocument();
-
+    expect(screen.getByText("Current Employees")).toBeInTheDocument();
+    expect(screen.getByText("Search:")).toBeInTheDocument();
+  });
 });
 
+jest.mock("react-redux", () => ({
+  ...jest.requireActual("react-redux"),
+  useSelector: jest.fn(),
+}));
